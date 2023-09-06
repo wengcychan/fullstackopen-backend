f1f4082cd16b383b1520b4999d5c6e95612e55fe
root <root@LAPTOP-01SJFTC3.localdomain>
2023-09-06 23:57:19 +0300

    add frontend

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
-const cors = require('cors')
 const app = express()
 
-app.use(cors())
-
 app.use(express.json())
+
+app.use(express.static('dist'))
 
 let notes = [
   {
